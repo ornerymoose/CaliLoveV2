4ee1d5a4912b2fa7b4f1db3ff78059b09e25897d
davidpardy <dpardy87@gmail.com>
2014-03-26 23:00:12 -0700

    gi now working

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_scdKj66X5MepCaCHOcJZAJ4l',
-  :secret_key      => 'sk_test_sEhQ2X4ZfeI26rhq8ufb8OuM'
-}
- 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
