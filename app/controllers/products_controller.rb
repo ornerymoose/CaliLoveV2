8a019d29885cd9fa1c09dba8dcca3c2f567e3126
davidpardy <dpardy87@gmail.com>
2014-04-10 23:16:52 -0700

    updated products controller, order show and line item partial with price

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,20 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def update
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product }
+        format.js
+        format.json { head :no_content }
+      else
+        format.html { redirect_to @product }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # GET /products/1/edit
 
   # POST /products
@@ -52,6 +66,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :price)
+      params.require(:product).permit(:name, :price, :size)
     end
 end
