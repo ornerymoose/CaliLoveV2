524e2f1466bc490c0c8a5c641c4fcd37ca55d45c
Kevin Musiorski <kmusiorski@connectedhealth.com>
2014-04-14 00:02:11 -0500

    get size out of params, pass new arg to add_product

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,7 +2,8 @@ class LineItemsController < InheritedResources::Base
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.add_product(product.id)
+    size = params[:product][:size]
+    @line_item = @cart.add_product(product.id, size)
 
     respond_to do |format|
       if @line_item.save
