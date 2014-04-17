06f2923d371ac3e683f468a145b95f730677550a
davidpardy <dpardy87@gmail.com>
2014-04-17 16:18:26 -0700

    paperclip added along with AWS

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,17 +1,32 @@
-ActiveAdmin.register Product do
+# ActiveAdmin.register Product do
 
   
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  permit_params :list, :of, :attributes, :on, :model, :name, :price
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+#   # See permitted parameters documentation:
+#   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#   #
+#   permit_params :list, :of, :attributes, :on, :model, :name, :price, :avatar
+#   #
+#   # or
+#   #
+#   # permit_params do
+#   #  permitted = [:permitted, :attributes]
+#   #  permitted << :other if resource.something?
+#   #  permitted
+#   # end
   
+# end
+
+ActiveAdmin.register Product do
+
+permit_params :list, :of, :attributes, :on, :model, :name, :price, :avatar
+
+form do |f|
+  f.inputs "Project Details" do
+    f.input :avatar, :required => false, :as => :file
+    # Will preview the image when the object is edited
+  end
+  f.actions
+ end
+
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,13 @@
 StripeAdmin::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => "california_love",
+      :access_key_id => "AKIAJNEZASJCN676EK5Q",
+      :secret_access_key => "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"
+    }
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
