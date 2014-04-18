3377c4bc843e54d47927afa2b2d6fb98127297e9
davidpardy <dpardy87@gmail.com>
2014-04-18 12:44:38 -0700

    update model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
 
   #has_attached_file :avatar#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
-  has_attached_file :avatar, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :default_url => "/images/normal/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   private
