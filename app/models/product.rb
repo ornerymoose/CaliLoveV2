6d10bf3bfd673d5160a114d06f0ccea0dd664324
davidpardy <dpardy87@gmail.com>
2014-04-18 12:40:37 -0700

    updated avatar model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,9 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  has_attached_file :avatar#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  #has_attached_file :avatar#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+  has_attached_file :avatar, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   private
