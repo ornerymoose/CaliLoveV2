233428430a076b1d25034842362ce8a47c526c68
davidpardy <dpardy87@gmail.com>
2014-04-18 09:38:52 -0700

    add 2nd arg

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -2,14 +2,15 @@ class CustomerMailer < ActionMailer::Base
   default from: "calove.help@gmail.com"
 
   #def registration_confirmation(user)
-  def received(order)
+  def received(order, email)
   	@cart = order
+  	@customer = email
   	#@customer = cust
     #@customer = user
     #attachments["rails.png"] = File.read("#{Rails.root}/public/images/rails.png")
     #mail to: "dpardy87@gmail.com"
     #mail(:to => "#{user}", :subject => "Your California Love Order")
-    mail to: @customer_id.email
+    mail to: email
   end
 
   # def email_sent(user)
