c06fbbfaa4f2c30a8ee5166dfab117cf947e1db7
davidpardy <dpardy87@gmail.com>
2014-04-18 01:35:14 -0700

    added inputfields

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -4,6 +4,8 @@ permit_params :list, :of, :attributes, :on, :model, :name, :price, :avatar
 
 form do |f|
   f.inputs "Project Details" do
+    f.input :name
+    f.input :price
     f.input :avatar, :required => false, :as => :file
     # Will preview the image when the object is edited
   end
