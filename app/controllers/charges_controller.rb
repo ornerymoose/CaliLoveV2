93835993177ce88d22404124af49a3e268d1b2b9
davidpardy <dpardy87@gmail.com>
2014-04-18 09:55:25 -0700

    add .email

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -35,7 +35,7 @@ class ChargesController < ApplicationController
 
     respond_to do |format|
       #CustomerMailer.registration_confirmation(@customer.email).deliver
-      CustomerMailer.received(@cart, @customer).deliver
+      CustomerMailer.received(@cart, @customer.email).deliver
       format.html { redirect_to charge_path(@cart, {customer_id: @customer.id}) }
       format.json { head :ok }
     end
