77a291da954d99b4a34f502c828d1fe7e7f04904
davidpardy <dpardy87@gmail.com>
2014-04-18 09:59:19 -0700

    live keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_scdKj66X5MepCaCHOcJZAJ4l',
-  :secret_key      => 'sk_test_sEhQ2X4ZfeI26rhq8ufb8OuM'
+  :publishable_key => 'pk_live_MuRFF8J7CUEKjVyo5P9z3S4Q',
+  :secret_key      => 'sk_live_TvapFpc2EPLhXwXt9ou3TI9t'
 }
  
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
