25e970bfb922e6c23eabc2328957760a149c114f
davidpardy <dpardy87@gmail.com>
2014-04-23 10:50:55 -0700

    updated send email subject and added back in test keys

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,10 +1,9 @@
 class CustomerMailer < ActionMailer::Base
   default from: "calove.help@gmail.com"
 
-  #def registration_confirmation(user)
   def received(order, email)
   	@cart = order
   	@customer = email
-    mail to: email
+    mail(to: email, subject: "Thank you for shopping with California Love")
   end
 end
